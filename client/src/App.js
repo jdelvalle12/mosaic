f188998862932070ad900dfde6aca0d05cccf15d
Jose Del Valle <jdelvalle88@live.com>
2025-10-23 16:09:14 -0400

    adding scroll to top feature again

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ScrollToTop from './ScrollToTop';
+import ScrollToTop from './components/ScrollToTop';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Logo from "./components/Logo";
