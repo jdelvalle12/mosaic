e04b11103a9e2f0a7f1ed1643eb74205812b5e82
Jose Del Valle <jdelvalle88@live.com>
2026-05-13 19:23:57 -0400

    updated logo and favicon

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import '../App.css';
+import MosaicLogo from '../images/mosaic-logo.png';
 import './Logo.css';
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <div className="logo">
-        {/* Add the SVG arc here */}
-        <svg className="arc" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          {/* The arc path goes from A to Z, passing over "uri" */}
-          {/* <path d="M 10 90 Q 30 30 50 10 Q 10 20 180 10 Q 15 20 17 100" fill="transparent" stroke="aqua" stroke-width="20" /> */}
-        </svg>
-        {/* Display the letters A and Z */}
-        {/* <span>A</span>uri<span>z</span>on */}
-      </div>
-      <Link className="no-underline flex items-center" to="/">
-        <span className="logo-span bg-aqua text-black bold px-1">M</span>OSAIC
+      <Link className="logo-link" to="/">
+        <img
+          src={MosaicLogo}
+          alt="Mosaic Digital Creations Logo"
+          className="mosaic-logo"
+        />
+
+        <span className="logo-text">OSAIC</span>
       </Link>
     </div>
   );
